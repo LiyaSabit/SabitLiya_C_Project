055d4e43bd7fe3d07858d314b5db5774e3305533
LiyaSabit <90717232+LiyaSabit@users.noreply.github.com>
2024-11-09 16:52:24 +0500

    Task 1 in Python

--- assesment/task_1.py
+++ assesment/task_1.py
@@ -1,10 +1,34 @@
-
 def main():
     num = int(input("Enter a number: "))
     number(num)
 
+    name = input("Enter a name: ")
+    greeting(name)
+
+    my_array = []
+    size = int(input("What is the length of the array? "))
+    for i in range(size):
+        elem = int(input(f"Enter element {i + 1}: "))
+        my_array.append(elem)
+    multiples(my_array)
+
+
 def number(num):
     if num > 7:
         print("Hello")
+
+
+def greeting(name):
+    if name == 'John':
+        print(f"Hello, {name}")
+    else:
+        print("There is no such name")
+
+
+def multiples(array):
+    for elem in array:
+        if elem % 3 == 0:
+            print(elem)
+
 
 main()
